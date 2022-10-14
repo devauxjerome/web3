9e6b50bd9b54c4d1ea54e8cae2b30b6fd9da74b7
Jérôme Devaux <jerome.devaux@student.vinci.be>
2022-10-14 09:15:32 +0200

    2.11

--- exercices/module4/2.6-2.10/src/App.js
+++ exercices/module4/2.6-2.10/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -18,6 +19,18 @@ const handleNumberChange  = (event) => {
   setNewNumber(event.target.value)
 }
 
+
+const hook = () => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+}
+
+useEffect(hook, [])
 
 
 const addName = (event) => {
